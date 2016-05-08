d7c00924f7f8749b018747c5d339979457d9fc87
Haribabu Pa <haribabupa@live.com>
2016-05-08 23:03:45 +0530

    Message formated

--- services/contactus.php
+++ services/contactus.php
@@ -14,7 +14,7 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 $headers .= 'From: <webmaster@medpalplus.com>' . "\r\n";
 
-if(mail('support@medpalplus.com', $name, $message + '<br><br>' + $email, $headers)){
+if(mail('support@medpalplus.com', $name, $message.'<br><br>'.$name.'<br>('.$email.')', $headers)){
     echo 'sent';
 }else{
     echo 'failed';
