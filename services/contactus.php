0b48f15b730676bb1d6de2a6de8ea42bc119ede5
Haribabu Pa <haribabupa@live.com>
2016-05-08 23:07:33 +0530

    header updated

--- services/contactus.php
+++ services/contactus.php
@@ -12,7 +12,7 @@ $message = strip_tags($_POST['message']);
 
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers .= 'From: <webmaster@medpalplus.com>' . "\r\n";
+$headers .= 'From: <'.$email_id.'>' . "\r\n";
 
 if(mail('support@medpalplus.com', $name, $message.'<br><br>'.$name.'<br>('.$email_id.')', $headers)){
     echo 'sent';
