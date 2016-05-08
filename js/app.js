62ef45095fd0b677b513a1f935a164c31bba52e8
Haribabu Pa <haribabupa@live.com>
2016-05-08 22:51:58 +0530

    contact us ready

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-    $('#send_message').bind('click', function (){
-        $.ajax('')
-    })
-})
+    $("#myform").on('submit', function(e){
+        e.preventDefault();
+        sendData();
+    });
+    
+});
+
+
+function sendData(){
+    var postData = { name: $('#name').val(), email:$('#email').val(), message:$('#message').val() }
+    console.log(postData);
+    $.ajax({
+      method: "POST",
+      url: "services/contactus.php",
+      data: postData
+    }).done(function( msg ) {
+                console.log( msg );
+            });
+}
--- services/contactus.php
+++ services/contactus.php
@@ -1,10 +1,16 @@
 <?php
 
-$email_id = @$_POST['email'];
-$name = @$_POST['name'];
-$message = @strip_tags($_POST['message']);
 
-if(mail('support@medpalplus.com', $name, $message)){
+if(!isset($_POST['email']) ||!isset($_POST['name']) ||!isset($_POST['message']) ){
+    echo 'failed';
+    exit(1);
+}
+
+$email_id = $_POST['email'];
+$name = $_POST['name'];
+$message = strip_tags($_POST['message']);
+
+if(mail('support@medpalplus.com', $name, $message + '<br><br>' + $email)){
     echo 'sent';
 }else{
     echo 'failed';
