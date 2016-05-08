9d737f8d45af46efa06ff2cb25c88c412706e2b7
Haribabu Pa <haribabupa@live.com>
2016-05-09 00:26:03 +0530

    rest api added

--- services/contactus.php
+++ services/contactus.php
@@ -3,13 +3,28 @@
 var_dump($_REQUEST);
 
 if(!isset($_REQUEST['email']) ||!isset($_REQUEST['name']) ||!isset($_REQUEST['message']) ){
-    echo 'failed';
-    exit(1);
+
+    $postdata = file_get_contents("php://input");
+    if (isset($postdata)) {
+        $request = json_decode($postdata);
+        if(isset($request->email)){
+            $username = $request->email;
+            $email_id = $request->email;
+            $name = $request->name;
+            $message = strip_tags($request->message);
+        }else{
+            echo 'failed';
+        }
+    }else{
+        echo 'failed';
+    }
+  
+}else{
+    $email_id = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $message = strip_tags($_REQUEST['message']);
 }
 
-$email_id = $_REQUEST['email'];
-$name = $_REQUEST['name'];
-$message = strip_tags($_REQUEST['message']);
 
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
